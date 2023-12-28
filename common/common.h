0da265a78da7e678288443ea43de7eb5e0f2780a
uyplayer <32848979@qq.com>
2023-12-28 21:27:33 +0800

    uncommit

--- common/common.h
+++ common/common.h
@@ -8,21 +8,20 @@
 
 
 
-#include <stdio.h>
+#include <cstdio>
 #include <iostream>
 #include <cuda_runtime.h>
 #include <cuda.h>
 
 
-static void HandleError( cudaError_t err,
-                         const char *file,
-                         int line ) {
+static void HandleError( cudaError_t err,const char *file,int line ) {
     if (err != cudaSuccess) {
         printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
                 file, line );
         exit( EXIT_FAILURE );
     }
 }
+
 #define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
 
 
@@ -33,4 +32,4 @@ static void HandleError( cudaError_t err,
 
 
 
-#endif //CUDA_NOTEBOOK_COMMON_H
+#endif
