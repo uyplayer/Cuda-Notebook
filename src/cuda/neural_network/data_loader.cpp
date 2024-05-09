659d41087c21d86ee310ea511810605ebf1ad965
uyplayer <32848979@qq.com>
2024-05-09 22:48:35 +0800

    add csv

--- src/cuda/neural_network/data_loader.cpp
+++ src/cuda/neural_network/data_loader.cpp
@@ -4,22 +4,21 @@
 #include "rapidcsv.h"
 
 
-
 class CSVDataLoader {
 public:
-    explicit CSVDataLoader(std::string filename): filename(std::move(filename)){
+    explicit CSVDataLoader(std::string filename) : filename(std::move(filename)) {
         if (this->filename.find(".csv") == std::string::npos) {
             std::cerr << "File is not in CSV format" << std::endl;
         }
     }
+
     ~CSVDataLoader() = delete;
-    rapidcsv::Document load_csv(){
+
+    rapidcsv::Document load_csv() {
         rapidcsv::Document doc(filename, rapidcsv::LabelParams(),
                                rapidcsv::SeparatorParams(),
                                rapidcsv::ConverterParams(true));
     }
-
-
 
 
 private:
