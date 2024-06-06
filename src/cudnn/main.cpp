7e2d50325996ff04fb973ea48ef5a810a225a1f4
uyplayer <32848979@qq.com>
2024-06-06 23:44:47 +0800

    add cudnn

---
+++ src/cudnn/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by uyplayer on 2024-06-06.
+//
+
+
+
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello cudnn !" << std::endl;
+    std::cout << "Cudnn is a GPU-accelerated library for deep neural networks." << std::endl;
+    return 0;
+}
