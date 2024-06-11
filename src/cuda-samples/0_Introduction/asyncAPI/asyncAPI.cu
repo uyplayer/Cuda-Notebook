656a74875db2c64e83838c98f1566260b0d9c07b
uyplayer <32848979@qq.com>
2024-06-12 00:35:58 +0800

    add cuda samples

---
+++ src/cuda-samples/0_Introduction/asyncAPI/asyncAPI.cu
@@ -0,0 +1,14 @@
+//
+// Created by uyplayer on 2024-06-11.
+//
+
+#include "asyncAPI.cuh"
+#include <iostream>
+
+
+
+
+int main() {
+    std::cout << "Hello, asyncAPI!" << std::endl;
+
+}
---
+++ src/cuda-samples/0_Introduction/asyncAPI/asyncAPI.cuh
@@ -0,0 +1,33 @@
+//
+// Created by uyplayer on 2024-06-11.
+//
+
+#ifndef ASYNCAPI_CUH
+#define ASYNCAPI_CUH
+
+
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <iostream>
+#include <cuda_profiler_api.h>
+
+class asyncAPI {
+
+
+private:
+    int devID ;
+    cudaDeviceProp prop;
+
+
+
+
+
+
+
+
+
+};
+
+
+
+#endif //ASYNCAPI_CUH
